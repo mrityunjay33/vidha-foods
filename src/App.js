10efa5defc354769080d25940028b78100825eeb
Mrityunjay Kumar <mk10399@gmail.com>
2024-03-23 22:38:26 +0530

    Project Structure Initialised

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Vidha Foods App</h1>
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>Home</div>
+        <div>Products</div>
+        <div>About</div>
+    </div>
+  )
+}
+
+export default Navbar
